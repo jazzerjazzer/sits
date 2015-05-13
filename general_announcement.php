32d94bfd6cd37ddd06f82d6fde3d439c492ffeb1
Sinem Sav <sinem.sav@ug.bilkent.edu.tr>
2015-05-13 18:46:21 +0300

    base in aynısı şuan

---
+++ general_announcement.php
@@ -0,0 +1,123 @@
+<html>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<meta http-equiv="Content-Type" content="text/HTML; charset=utf-8" />
+
+	<style style="text/css">
+	
+
+	#header {
+		border-radius:10px;
+		border: 5px solid #520052;
+		margin-top: 15px;
+		color:white;
+	}
+	#nav {
+		padding-top: 15px;
+		background-color:#838E91;
+		height:500px;
+		width:20%;
+		float:left;
+		margin-top:60px;
+	}
+	.nav_button {
+		text-align:center;
+		text-decoration: none;
+		font-size: 120%;
+		font-family: sans-serif;
+		color: #FFF;
+		-webkit-font-smoothing: antialiased;
+		background: #EE8532;
+		padding: 10px 20px;
+		margin:5px 0px;
+		display: inline-block;
+		white-space: nowrap;
+		-moz-border-radius: 5px;
+		-webkit-border-radius: 5px;
+		border-radius: 2px;
+		border: 2px solid rgba(255, 255, 255, 0.23);
+		-moz-box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.14);
+		-webkit-box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.14);
+		box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.14);
+		-webkit-transition: all 0.1s ease;
+		-ms-transition: all 0.1s ease;
+		-moz-transition: all 0.1s ease;
+		-o-transition: all 0.1s ease;
+		transition: all 0.1s ease;
+		width:225px;
+		height:50px;
+	}
+	.nav_button:hover {
+		background: #C46C28;
+	}
+	ul {
+    	list-style-type: none;
+    	margin: 0;
+    	padding: 0;
+	}
+	
+
+	#right_content_area{
+		width:79%;
+		margin-top:20px;
+		float:right;
+	}
+
+	#container{
+		width:99%;
+		height:400px;
+	}
+	
+	h1 {
+ 		text-align: center;
+	}	
+	body{
+		background-color:#00A4CE; 
+	}
+
+	#user_info{
+		border: 5px solid #520052;
+		color:white;
+	}
+	
+</style>
+	<body>
+		<!-- Top part-->
+		<div id="header">
+			<img src="logo.jpg" width="65" height="65" align="left" vspace="2" hspace="5">
+			<h1> Student Internship Tracking System </h1>
+		</div> 
+		
+
+	<div id="container">
+		<div id="right_content_area"> 
+			
+		</div>
+
+		<div id="nav">
+			<div id="user_info">
+				<?php
+					@session_start();
+					$usr = $_SESSION["user_name"];
+					echo "<p>$usr</p>";
+					echo "<p>CS</p>";
+				?>
+				<p>Logout</p>
+			</div>
+
+			<div id="menu_buttons">
+				<ul>
+				  <li><a href="#" class="nav_button">My Applications</a></li>
+				  <li><a href="#" class="nav_button">Quotas</a></li>
+				  <li><a href="#" class="nav_button">Companies</a></li>
+				  <li><a href="#" class="nav_button">Anouncements</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
